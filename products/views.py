96847489fb9cc0c592bbcf2cb637f0fc3a29104b
Damilola Adekoya <dharmykoya38@gmail.com>
2019-09-11 15:26:47 +0100

    setup database and download db browser for sqlite

--- products/views.py
+++ products/views.py
@@ -4,5 +4,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-  return HttpResponse(' ')
+  return HttpResponse('Hello World')
 
+def new(request):
+  return HttpResponse('A new product')
